e4174233dfc18428954203f03882e53045ef86a9
root <root@srv701763.hstgr.cloud>
2025-05-17 01:10:48 +0000

    Admin notification changes from the server

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
+    'corsheaders',
     'orders',
     'products',
     'users'
@@ -59,11 +60,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+        "http://localhost:5173",
 ]
 
 REST_FRAMEWORK = {
